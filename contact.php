bf20d6915b3c0e1bf8dd5ea253e06a3274d4a2bf
Deovrat Bharadwaj <deovrat247@gmail.com>
2024-11-18 01:09:00 +0530

    push contact changes

--- contact.php
+++ contact.php
@@ -77,45 +77,33 @@
                               </div>
                             </div>
                           </div>
-                          <div class="vc_row wpb_row vc_row-fluid vc_custom_1475847858282"><div class="wpb_column vc_column_container vc_col-sm-3"><div class="vc_column-inner">
-                              <div class="wpb_wrapper">
-                          </div>
-                          </div>
-                          </div>
-                            
-    <!--        <div class="wpb_column vc_column_container vc_col-sm-6">-->
-    <!--                          <div class="vc_column-inner">-->
-    <!--                          <div class="wpb_wrapper">-->
-    <!--                              <h2 style="text-align: center" class="vc_custom_heading section-heading vc_custom_1475848393354" >-->
-    <!--                                  Drop us a line</h2>-->
-    <!--                                  <div role="form" class="wpcf7" id="wpcf7-f323-p3427-o1" lang="en-US" dir="ltr">-->
-    <!--                                      <div class="screen-reader-response">-->
-    <!--                                      </div>-->
-    <!--                                      <form action="" method="post" class="wpcf7-form demo" novalidate="novalidate">-->
-    <!--                                          <div style="display: none;"> -->
-    <!--                                          <input type="hidden" name="_wpcf7" value="323" /> -->
-    <!--                                          <input type="hidden" name="_wpcf7_version" value="5.1.3" /> -->
-    <!--                                          <input type="hidden" name="_wpcf7_locale" value="en_US" /> -->
-    <!--                                          <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f323-p3427-o1" />-->
-    <!--                                          <input type="hidden" name="_wpcf7_container_post" value="3427" />-->
-    <!--                                          </div>-->
-    <!--                                          <p><span class="wpcf7-form-control-wrap your-name">-->
-    <!--<input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="YOUR NAME" />-->
-    <!--</span><br /> -->
-    <!--<span class="wpcf7-form-control-wrap your-email">-->
-    <!--<input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="YOUR EMAIL" />-->
-    <!--</span><br /> -->
-    <!--<span class="wpcf7-form-control-wrap your-message">-->
-    <!--    <textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="THE MESSAGE">-->
-    <!--    </textarea>-->
-    <!--    </span><br /> -->
-    <!--    <input type="submit" value="Send Message" class="wpcf7-form-control wpcf7-submit" />-->
-    <!--    </p>-->
-    <!--    <div class="wpcf7-response-output wpcf7-display-none"></div>-->
-    <!--    </form>-->
-    <!--    </div>-->
-    <!--    </div>-->
-    <!--    </div>-->
+                          <div class="wpb_column vc_column_container vc_col-sm-6">
+                            <div class="vc_column-inner">
+                                <div class="wpb_wrapper">
+                                    <h2 style="text-align: center" class="vc_custom_heading section-heading vc_custom_1475848393354">
+                                        Drop us a line</h2>
+                                    <div role="form" class="wpcf7" id="wpcf7-f323-p3427-o1" lang="en-US" dir="ltr">
+                                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="wpcf7-form">
+                                            <p>
+                                                <span class="wpcf7-form-control-wrap your-name">
+                                                    <input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="YOUR NAME" required />
+                                                </span><br />
+                                                <span class="wpcf7-form-control-wrap your-email">
+                                                    <input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="YOUR EMAIL" required />
+                                                </span><br />
+                                                <span class="wpcf7-form-control-wrap your-message">
+                                                    <textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="THE MESSAGE" required></textarea>
+                                                </span><br />
+                                                <input type="submit" value="Send Message" class="wpcf7-form-control wpcf7-submit" />
+                                            </p>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="wpb_column vc_column_container vc_col-sm-12" style="margin-bottom:-40px">
             <iframe src="https://www.google.com/maps/embed?pb=" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -124,7 +112,31 @@
             </div>
                          </div></div></section>
 
-                      	
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['your-name'];
+    $email = $_POST['your-email'];
+    $message = $_POST['your-message'];
+    
+    $to = "info@globalrepairgeek.com";
+    $subject = "New Contact Form Submission from " . $name;
+    
+    $email_content = "Name: " . $name . "\n";
+    $email_content .= "Email: " . $email . "\n\n";
+    $email_content .= "Message:\n" . $message . "\n";
+    
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+    
+    if(mail($to, $subject, $email_content, $headers)) {
+        echo "<script>alert('Thank you for your message. We will get back to you soon!');</script>";
+    } else {
+        echo "<script>alert('Sorry, there was an error sending your message. Please try again later.');</script>";
+    }
+}
+?>
+
 	<?php include "includes/footer.php" ?>
 </body>
 </html>
